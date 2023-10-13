c256bdcafde909b6e6f72b6027ce5ffc095ea8b3
Tarun-Raikwar <raikwartarun72@gmail.com>
2023-10-13 17:04:59 +0530

    calender done

--- src/components/Calender/Calender.jsx
+++ src/components/Calender/Calender.jsx
@@ -10,42 +10,44 @@ const Calender = ({handle_popup, type}) => {
   const [todayEventVis, setTodayEventVis] = useState(false);
   const eventref = useRef(null);
 
-  let date_today = new Date();
-  let firstDay = new Date(date_today.getFullYear(), date_today.getMonth(), 1);    
-  const blank_days1 = Array.from({ length: firstDay.getDay() }, (_, index) => index + 1);
-  const main_days = Array.from({ length: new Date(date_today.getFullYear(), date_today.getMonth(), 0).getDate()}, (_, index) => index + 1);
-  const blank_days2 = Array.from({ length: (35 - main_days.length - blank_days1.length) }, (_, index) => index + 1);
-
-
   const [check, setCheck] = useState(false);
   const [birthday, setBirthday] = useState(false);
   const [school, setschool] = useState(false);
   const [tasks, setTasks] = useState(false);
 
+  let dateToday = new Date();
+  let firstDay = new Date(dateToday.getFullYear(), dateToday.getMonth(), 1);    
+  const beforeExtra = Array.from({ length: firstDay.getDay() }, (_, index) => index + 1);
+  const mainDays = Array.from({ length: new Date(dateToday.getFullYear(), dateToday.getMonth(), 0).getDate()}, (_, index) => index + 1);
+  const afterExtra = Array.from({ length: (35 - mainDays.length - beforeExtra.length) }, (_, index) => index + 1);
 
-  const [event_array, set_event_array] = useState(new Array(main_days.length).fill([])); 
+
+const [event_array, set_event_array] = useState(Array.from({ length: mainDays.length}, () => new Array()));
+
+  
 
   useEffect(() => {
     window.addEventListener("mousemove", (event) => {
       setMousePos({x: event.clientX, y: event.clientY});
     })
 
-
-    events_rare.responseObject.events.map((events) => {
-      const d = new Date(events.date);
+    events_rare.responseObject.events.map((event) => {
+      const d = new Date(event.date);
       const today_date = d.getDate();
       const today_month = d.getMonth();
       const today_year = d.getFullYear();
-      console.log(events.date, today_date, today_month, today_year, date_today.getMonth(), date_today.getFullYear());
+      console.log(event.date, today_date, today_month, today_year, dateToday.getMonth(), dateToday.getFullYear());
       
-      if(today_month === date_today.getMonth() && today_year === date_today.getFullYear()){
-        console.log(events)
-        if (!event_array[today_date-1]) {
-          event_array[today_date-1] = [];
-        }
-        event_array[today_date-1].push(events);
+      if(today_month === dateToday.getMonth() && today_year === dateToday.getFullYear()){
+        console.log(typeof(today_date))
+        let copy = event_array;
+        console.log(copy);
+        copy[today_date-1].push(event);
+        console.log(copy[today_date-1]);
+        set_event_array(copy);
       }
     }) 
+
     
     console.log(event_array)
   }, [])
@@ -61,8 +63,8 @@ const Calender = ({handle_popup, type}) => {
             setTodayEventVis(true);
 
             if(eventref.current !== null){
-              eventref.current.style.left = mousePos.x -6 + "px";
-              eventref.current.style.top = mousePos.y -130 + "px";
+              eventref.current.style.left = mousePos.x - 6 + "px";
+              eventref.current.style.top = mousePos.y - 130 + "px";
             }
         }}
         onMouseOut={() => {
@@ -95,26 +97,31 @@ const Calender = ({handle_popup, type}) => {
         <div className="day day_name">Sat</div>
 
         {
-          blank_days1.map(() => {
+          beforeExtra.map(() => {
             return <div className="day"></div>
           })
         }
 
         {
-          main_days.map((number) => {
+          mainDays.map((number) => {
             return day(number, event_array[number-1]);
           })
         }
 
         {
-          blank_days2.map(() => {
+          afterExtra.map(() => {
             return <div className="day"></div>
           })
         }
 
       </div>
       
-      <div className="event_container" ref={eventref}>
+      <div 
+        className="event_container" 
+        ref={eventref}
+        onMouseEnter={() => setTodayEventVis(true)}
+        onMouseLeave={() => setTodayEventVis(false)}
+      >
         {todayEventVis && 
           <div className="events">
             {todayEvent && 
@@ -128,7 +135,8 @@ const Calender = ({handle_popup, type}) => {
         }
       </div>
         
-      <img className="puka" src={require("../../assets/pucka.png")} alt="" />
+      <img className="puka" src={require("../../assets/pucka.png")} alt="" 
+      />
     </div>
   );
 }
